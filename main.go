b6d7defa7930c6c14e312de976f9ac076c458793
Ashraff5 <142718140+Ashraff5@users.noreply.github.com>
2024-11-16 14:33:53 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,30 +5,21 @@ import (
 	"net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
+func about(w http.ResponseWriter, r *http.Request) {
 	// Render the home html page from static folder
-	http.ServeFile(w, r, "static/home.html")
-}
-
-func coursePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page
-	http.ServeFile(w, r, "static/courses.html")
-}
-
-func aboutPage(w http.ResponseWriter, r *http.Request) {
-	// Render the about html page
 	http.ServeFile(w, r, "static/about.html")
 }
 
-func contactPage(w http.ResponseWriter, r *http.Request) {
-	// Render the contact html page
-	http.ServeFile(w, r, "static/contact.html")
+func project(w http.ResponseWriter, r *http.Request) {
+	// Render the course html page
+	http.ServeFile(w, r, "static/project.html")
 }
+
 
 func main() {
 
-	http.HandleFunc("/about", homePage)
-	http.HandleFunc("/project", coursePage)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/project", project)
 	
 
 	err := http.ListenAndServe("0.0.0.0:8080", nil)
